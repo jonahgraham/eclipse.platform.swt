e29be00164a919b3f1ed3539a4822e371481be29
Jonah Graham <jonah@kichwacoders.com>
2025-12-05 23:10:41 -0500

    fake win32 edit

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/Platform.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/Platform.java
@@ -14,7 +14,7 @@
 package org.eclipse.swt.internal;
 
 import org.eclipse.swt.internal.win32.version.*;
-// fake edit 3
+// fake edit 4
 public class Platform {
 	public static final String PLATFORM = "win32"; //$NON-NLS-1$
 
